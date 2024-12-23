a2ce9ac41ee924f5ab8019a24f5c78fb7e8e3199
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-12-23 00:40:34 -0700

    `Removed unused imports and simplified page component`

--- frontend/src/app/ContactUs/page.tsx
+++ frontend/src/app/ContactUs/page.tsx
@@ -1,19 +1,7 @@
-/**
- * The function `page` renders a React component `CarBookingSection` within a `div` element.
- * @returns The `page` component is being returned, which includes the `CarBookingSection` component
- * wrapped inside a `div` element.
- */
 import React from "react";
-import CarBookingSection from "../Components/ContactUs";
-import NavBar from "../Components/NavBar";
 
 const page = () => {
-  return (
-    <div>
-      {" "}
-      <CarBookingSection />
-    </div>
-  );
+  return <div>page</div>;
 };
 
 export default page;
